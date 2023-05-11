5f605930c51adcee5f3d7ab4720b38451843fb43
DonRyu <alpharyu1031@gmail.com>
2023-05-11 11:34:18 -0700

    navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navBar">
+    <a href="">Home</a>
+    <a href="">Shop</a>
+    <a href="">About</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style>
+.navBar {
+  background-color: purple;
+}
+.navBar a {
+  color: white;
+  font-size: 40px;
+  text-decoration: none;
+  padding: 15px;
+}
+</style>
